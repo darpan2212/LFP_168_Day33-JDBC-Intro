13e6a0927df5a5ef55b097f83e41ad7b9d22c318
Darpan <darpan.madlani@gmail.com>
2022-09-17 20:41:13 +0530

    New employee details are added in DB

---
+++ src/com/bl/jdbc/TestImpl.java
@@ -0,0 +1,16 @@
+package com.bl.jdbc;
+
+public class TestImpl {
+
+	public static void main(String[] args) {
+		
+		String dept_id = "2,3";
+		
+		String[] dept_ids = dept_id.split(",");
+		
+		for (int i = 0; i < dept_ids.length; i++) {
+		}
+		
+	}
+	
+}
--- src/com/bl/jdbc/service/PayrollServiceImpl.java
+++ src/com/bl/jdbc/service/PayrollServiceImpl.java
@@ -4,16 +4,20 @@ public class PayrollServiceImpl {
 
 	public static void main(String[] args) {
 
-		int emp_id = 2;
-		double basic_pay = 3500000;
-
 		PayrollService service = new PayrollService();
 
 		service.getEmpData();
 
-		service.updateSalary(emp_id, basic_pay);
-
-		service.getEmpDataByJoinDate("2018-01-01", "2018-12-31");
+		System.out.println("------------------------------------------");
+		/*
+		 * service.updateSalary(emp_id, basic_pay);
+		 * 
+		 * service.getEmpDataByJoinDate("2018-01-01", "2018-12-31");
+		 */
+		
+		service.addEmpData();
+		System.out.println("------------------------------------------");
+		service.showEmpDeptPayrollData();
 	}
 
 }
